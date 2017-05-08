7b2f1ffb37f573c457399cae8f255c2785fb7d41
jzteam <zhujizhi1012@163.com>
2017-05-08 09:41:07 +0800

    bigdecimal

---
+++ src/cn/jzteam/base/TestBigDecimal.java
@@ -0,0 +1,11 @@
+package cn.jzteam.base;
+
+public class TestBigDecimal {
+
+    public static void main(String[] args) {
+        float a = 1.0f;
+        float b = 0.01f;
+        System.out.println(a - b);
+    }
+
+}
