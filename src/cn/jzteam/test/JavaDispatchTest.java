96fcf496520ad9f0ece44d369df5d6da76460834
jzteam <zhujizhi1012@163.com>
2017-10-13 00:41:39 +0800

    不能调用父类的父类了

--- src/cn/jzteam/test/JavaDispatchTest.java
+++ src/cn/jzteam/test/JavaDispatchTest.java
@@ -1,16 +1,40 @@
 package cn.jzteam.test;
 
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.MethodType;
+
 public class JavaDispatchTest {
 
     static class Human {
-        public void say() {
+        public void say() throws Throwable {
             System.out.println("human...");
+        }
+        public void eat(){
+            System.out.println("human11111 eat...");
         }
     }
 
     static class Man extends Human {
-        public void say() {
+        public void say() throws Throwable {
+            
             System.out.println("man...");
+        }
+        
+        public void eat(){
+            System.out.println("man222222 eat...");
+        }
+    }
+    
+    static class Person extends Man {
+        public void say() throws Throwable {
+            MethodType methodType = MethodType.methodType(void.class);
+            MethodHandle findSpecail = MethodHandles.lookup().findSpecial(Human.class, "eat", methodType,getClass());
+            findSpecail.invoke(this);
+            System.out.println("Person...");
+        }
+        public void eat(){
+            System.out.println("Person33333 eat...");
         }
     }
 
@@ -41,13 +65,23 @@ public class JavaDispatchTest {
         System.out.println("object..." + a);
     }
 
-    public static void main(String[] args) {
-        JavaDispatchTest dispatchTest = new JavaDispatchTest();
-        Human man = new Man();
-        Human woman = new Woman();
-        dispatchTest.say(man);
-        dispatchTest.say(woman);
-
+    public static void main(String[] args) throws Throwable {
+//        JavaDispatchTest dispatchTest = new JavaDispatchTest();
+//        Human man = new Man();
+//        Human woman = new Woman();
+//        dispatchTest.say(man);
+//        dispatchTest.say(woman);
+        
+        MethodType methodType = MethodType.methodType(void.class);
+//        MethodHandle findVirtual = MethodHandles.lookup().findVirtual(Human.class, "say", methodType);
+//        findVirtual.invoke(new Person());
+        
+        // no private access for invokespecial: class cn.jzteam.test.JavaDispatchTest$Human, from cn.jzteam.test.JavaDispatchTest
+        // findSpecial的调用，必须在
+//        MethodHandle findSpecail = MethodHandles.lookup().findSpecial(Human.class, "eat", methodType,Person.class).bindTo(new Person());
+//        findSpecail.invoke();
+        
+        new Person().say();
     }
 
 }
