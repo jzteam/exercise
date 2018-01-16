998c1ffb0ba5b69f1f7f649b22c03afbef4f3f3f
jzteam <zhujizhi1012@163.com>
2018-01-16 15:36:40 +0800

    二维码识别

--- src/cn/jzteam/utils/QRCodeUtil.java
+++ src/cn/jzteam/utils/QRCodeUtil.java
@@ -14,11 +14,18 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
 import java.util.HashMap;
 import java.util.Map;
 
 public class QRCodeUtil {
 
+    /**
+     * 生成二维码
+     * @param content
+     * @return
+     */
     public static String generateToBase64Str(String content){
         // 默认宽高都是200
         return generateToBase64Str(content,200,200);
@@ -53,6 +60,11 @@ public class QRCodeUtil {
         return null;
     }
 
+    /**
+     * 识别base64图片中的二维码
+     * @param base64Str
+     * @return
+     */
     public static String parseFromBase64Str(String base64Str){
         try {
             final byte[] bytes = new BASE64Decoder().decodeBuffer(base64Str);
@@ -73,8 +85,37 @@ public class QRCodeUtil {
         return null;
     }
 
+    /**
+     * 生成二维码，写入到文件中
+     * @param filePath
+     * @param content
+     * @param width
+     * @param height
+     * @return
+     */
+    public static String generateToFile(String filePath, String content,int width,int height){
+        String format = "png";// 图像类型
+        Map<EncodeHintType, Object> hints = new HashMap<>();
+        hints.put(EncodeHintType.CHARACTER_SET, "UTF-8");
+
+        try {
+            // 生成矩阵
+            BitMatrix bitMatrix = new MultiFormatWriter().encode(content,
+                    BarcodeFormat.QR_CODE, width, height, hints);
+            Path path = FileSystems.getDefault().getPath(filePath);
+            MatrixToImageWriter.writeToPath(bitMatrix, format, path);// 输出图像
+        } catch (Exception e) {
+            System.out.println("exception ...");
+        }
+        return null;
+    }
+
+    /**
+     * 能识别图片中的二维码
+     * @param filePath
+     * @return
+     */
     public static String parseFromFile(String filePath){
-//        String filePath = "/Users/oker/work/test/zxing.png";
         try {
             LuminanceSource source = new BufferedImageLuminanceSource(ImageIO.read(new File(filePath)));
             Binarizer binarizer = new HybridBinarizer(source);
@@ -93,9 +134,16 @@ public class QRCodeUtil {
     }
 
     public static void main(String[] args) {
-        String base64Str = generateToBase64Str("https://www.binance.com/?ref=10926868");
-        System.out.println("base64Str = "+base64Str);
-        final String url = parseFromBase64Str(base64Str);
-        System.out.println("解析得到：url="+url);
+        String url = "https://www.binance.com/?ref=10926868";
+        String filePath = "/Users/oker/work/test/zxingtest.png";
+        String filePath1 = "/Users/oker/work/test/testzxing.png";
+//        String base64Str = generateToBase64Str(url);
+//        System.out.println("base64Str = "+base64Str);
+//        url= parseFromBase64Str(base64Str);
+//        System.out.println("解析得到：url="+url);
+
+//        generateToFile(filePath,url,200,200);
+//        System.out.println("写入完成");
+        System.out.println("解析结果："+parseFromFile(filePath1));
     }
 }
