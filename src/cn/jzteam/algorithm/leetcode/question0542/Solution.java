bd4dabf1652f0f90c46c70d4a80d1cfa74bcaf28
Jzteam <zhujizhi1012@163.com>
2020-04-16 11:06:03 +0800

    leetCode0542 矩阵，广度优先算法

--- src/cn/jzteam/algorithm/leetcode/question0542/Solution.java
+++ src/cn/jzteam/algorithm/leetcode/question0542/Solution.java
@@ -2,6 +2,11 @@ package cn.jzteam.algorithm.leetcode.question0542;
 
 import com.alibaba.fastjson.JSON;
 
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.concurrent.LinkedBlockingQueue;
+
 /*
     给定一个由 0 和 1 组成的矩阵，找出每个元素到最近的 0 的距离。
 
@@ -40,6 +45,57 @@ import com.alibaba.fastjson.JSON;
     著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
 public class Solution {
+
+    // 广度优先：从0主动扩散找1
+    // 这个扩散有两个核心点：
+    // 1、每一次扩散，只找相邻的4个点（上下左右）未设置值的点，设置距离为当前点距离+1，然后该点就是下轮扩散的起点
+    // 2、每一轮扩散，都是所有起点同时开始扩展一次
+    // 这个规则解决了我的两个疑惑：
+    // 1、两个0中间的那个1的距离怎么确定？ 因为每次扩散都是距离+1，距离最近的0肯定是最先找到这个1，设置了值之后就不再变了。
+    // 2、从0扩散，只想到纵横，可能找不到1？ 因为每次扩散只走1步，而且是上下左右，所以能够全方位覆盖
+    public static int[][] updateMatrix(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        // 标记是否设置过值
+        int[][] flags = new int[m][n];
+        int[][] result = new int[m][n];
+        Queue<int[]> points = new LinkedBlockingQueue<>();
+
+        // 四个方向
+        int[][] dist = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+
+        // 找到第一轮的起点
+        for(int i=0;i<m;i++) {
+            for (int j=0;j<n;j++) {
+                if (matrix[i][j] == 0) {
+                    flags[i][j] = 1;
+                    // 第一轮的起点
+                    points.add(new int[]{i, j});
+                }
+            }
+        }
+        // O(mn)
+        while (!points.isEmpty()) {
+            int[] point = points.poll();
+            int x = point[0];
+            int y = point[1];
+            // 遍历四个方向的相邻点
+            for (int i=0;i<dist.length;i++) {
+                // 相邻点的坐标
+                int nx = x + dist[i][0];
+                int ny = y + dist[i][1];
+                // 没越界，且没设置过值，才设值
+                if (nx >= 0 && nx < m && ny >= 0 && ny < n && flags[nx][ny] != 1) {
+                    result[nx][ny] = result[x][y] + 1;
+                    flags[nx][ny] = 1;
+                    points.add(new int[]{nx, ny});
+                }
+            }
+        }
+        return result;
+    }
+
+
     // 动态规划：从1主动去找0
     // 任何为1的点到最近的0的距离为f(x,y)，那么可以概括成四种路径，就是
     // 只向左 + 只向上
@@ -51,7 +107,7 @@ public class Solution {
     // f(x,y) = 1 + min(f(x-1,j), f(x, j-1))
     // 边界条件是 当前点的值为0，则f(x,y)=0
 
-    public static int[][] updateMatrix(int[][] matrix) {
+    public static int[][] updateMatrix1(int[][] matrix) {
         int m = matrix.length;
         int n = matrix[0].length;
         int max = m + n;
@@ -109,7 +165,7 @@ public class Solution {
     // 个人方案，优化 速度 5%，内存100%
     // 找到所有为0的坐标，找到所有1的坐标
     // 两个坐标能计算出距离，2层循环找出最短距离
-    public static int[][] updateMatrix1(int[][] matrix) {
+    public static int[][] updateMatrix2(int[][] matrix) {
         int[][] data = new int[matrix.length][matrix[0].length];
         // 为0元素的坐标
         int[][] point0 = new int[matrix.length * matrix[0].length][2];
