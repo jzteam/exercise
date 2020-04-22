52760cd370b90d7100f1e993d9cb0106d4702fbf
Jzteam <zhujizhi1012@163.com>
2020-04-22 09:47:06 +0800

    深度优先算法-优化

--- src/cn/jzteam/algorithm/leetcode/question0199/Solution.java
+++ src/cn/jzteam/algorithm/leetcode/question0199/Solution.java
@@ -1,5 +1,7 @@
 package cn.jzteam.algorithm.leetcode.question0199;
 
+import com.alibaba.fastjson.JSON;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -24,7 +26,7 @@ import java.util.List;
  */
 public class Solution {
 
-    public List<Integer> rightSideView(TreeNode root) {
+    public static List<Integer> rightSideView(TreeNode root) {
         List<Integer> result = new ArrayList<>();
         if (root == null) {
             return result;
@@ -34,23 +36,29 @@ public class Solution {
         return result;
     }
     private static void dg(List<Integer> result, int index, TreeNode parentNode) {
-        // 对应这层级，先放左，后放右
-        if (parentNode.left != null) {
-            if (index > result.size() - 1) {
-                result.add(parentNode.left.val);
-            } else {
-                result.set(index, parentNode.left.val);
-            }
-            dg(result, index+1, parentNode.left);
-        }
+        // 对应这层级，先放右，后放左。（如果这层级已有元素，一定是右，左就不要set了）
+        // 如果list长度撑开了，表示每一层级一定都是最靠右的元素，不用再设置了
         if (parentNode.right != null) {
             if (index > result.size() - 1) {
                 result.add(parentNode.right.val);
-            } else {
-                result.set(index, parentNode.right.val);
             }
             dg(result, index+1, parentNode.right);
         }
+        if (parentNode.left != null) {
+            if (index > result.size() - 1) {
+                result.add(parentNode.left.val);
+            }
+            dg(result, index+1, parentNode.left);
+        }
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.right = new TreeNode(5);
+        root.right.right = new TreeNode(4);
+        System.out.println(JSON.toJSONString(rightSideView(root)));
     }
 }
 
